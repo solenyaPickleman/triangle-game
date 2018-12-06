661ac06b8ce47ac0457b2e0a8b0a35cdd155c0aa
solenyaPickleman <bradyadice@gmail.com>
2018-12-05 21:16:47 -0500

    began writing the play/solve portion of the program

--- triangle_game/src/triangle_game/core.clj
+++ triangle_game/src/triangle_game/core.clj
@@ -4,6 +4,7 @@
 
 (def result-rankings '("" "You're genius" "You're purty smart" "You're just plain dump" "You're just plain 'eg-noor-a-moose'"))
 
+;build game
 
 (defn get-peg "gets a peg or space" [is-peg]
   (if is-peg 0 1))
@@ -25,6 +26,7 @@
   (split-tree 1 game )
   )
 
+;display game
 (defn print-game-line "takes a list and prints out a line of the game"
   [line]
   (str (clojure.string/join "" (take (- 5 (count line)) (repeat " "))) (clojure.string/join " " line)))
@@ -32,8 +34,45 @@
 (defn print-game "print out a representation of the game" [game]
   (doseq [x game] (println (print-game-line x))))
 
+;play and solve game
+(defn get-moves "given a game board, gives all possible moves in the form of the potential game board"
+  [game]
+  (loop [index 0 moves ()]
+    (if (= index (- (count (flatten game)) 1))
+      moves ;at this point ,all have been looped through so return the vector of potential game boards
+      (recur
+        (+ index 1)
+        (if (= (nth game index) 1)
+          moves
+          (concat (moves (get-moves-for-hole index game)))
+          )))))
+
+
+(defn get-moves-for-hole "given a game board and specific hole, give all possible moves for that peg"
+    [hole game] (
+    (def row (which-row peg)))
+
+ (defn which-row "given a peg index, return integer corresponding to row 1-4"
+    [i]
+    (cond
+      (< i 1) 0
+      (< i 3) 1
+      (< i 6) 2
+      (< i 10) 3
+      :else 4)
+    )
+(defn get-surrounding "given a hole, get surrounding pegs #thats what she said"
+  [hole game]
+  )
+
+;main
 (defn -main
-  "I don't do a whole lot ... yet."
+  "Build a random board of Cracker Barrel peg solitaire and solve that board"
   [& args]
   (def game (start-game))
-  (print-game game))
+  (println "Initial board" )
+  (print-game game)
+  (get-peg-moves 2 game)
+  )
+
+
