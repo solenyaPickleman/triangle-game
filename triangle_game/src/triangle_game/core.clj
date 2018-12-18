70d50f0cdc35a3313a6a108b4cfd645d0b34242e
Brady Dice <bradyadice@gmail.com>
2018-12-18 00:04:01 -0500

    game mechanics finished- game will run one turn correctly

--- triangle_game/src/triangle_game/core.clj
+++ triangle_game/src/triangle_game/core.clj
@@ -55,6 +55,17 @@
     )
   )
 
+(defn which-element-in-game "given row and index, return overall index of peg in game"
+  [row index]
+  ( + index (cond
+              (= row 0) 0
+              (= row 1) 1
+              (= row 2) 3
+              (= row 3) 6
+              :else 10
+              ))
+  )
+
 (defn get-moves-samerow "given a hole, row, and game board, returns all moves on the same row. Moves are returned as possible game boards"
   [hole row game]
   (if (< row 2)
@@ -91,52 +102,99 @@
 
 (defn get-moves-above "given a hole, row, and game board, returns all moves from above the row. Moves are returned as possible game boards"
   [hole row game]
-  (if (= row 0)
-    '()
+  (if (< row 2)
+    '() ; there are no moves in the first or second rows
     (do
       ;TODO write function that combines this and does the below ones as well... would save a lot of space. pass da function
       (def left-move (loop [
                             r row
                             index (which-element-in-row hole)
                             move '()]
-                       (if (or (< row 0) (< index 0) (= (count move) 3))
+                       (if (or (< r 0) (< index 0) (= (count move) 3))
                          (reverse move)
-                         (recur (- row 1) (- index 1) (conj move (nth (nth game row) index))))
+                         (recur (- r 1) (- index 1) (conj move (nth (nth game r) index))))
                        ))
       (def right-move (loop [
                             r row
                             index (which-element-in-row hole)
                             move '()]
-                       (if (or (>= row (count game )) (>= index (count (nth game row))) (= (count move) 3))
+                       (if (or (>= r (count game )) (>= index (count (nth game r))) (= (count move) 3))
                          (reverse move)
-                         (recur (- row 1) (+ index 1) (conj move (nth (nth game row) index))))
+                         (recur (- r 1) (+ index 1) (conj move (nth (nth game r) index))))
                        ))
-      (println left-move right-move)
-      ;TODO check validity and return possible game states
+      (def movelist (filter #(= 3 (count %)) (list right-move left-move)))
+      (loop [ moves (filter #(= 2 (reduce +
+                                          (let [g (flatten game)]
+                                            (list
+                                              (nth g (first %))
+                                              (nth g (second %))
+                                              (nth g (nth % 2))))))
+                            movelist )
+             new_games () ]
+        (if ( = (count moves) 0)
+          new_games
+          (recur (rest moves) (conj new_games (split-tree 1 (apply list
+                                                                   (assoc
+                                                                     (assoc  (assoc (vec (flatten game)) (first (first moves)) 1) (second (first moves)) 0)
+                                                                     (nth (first moves) 2) 0)
+                                                                   ))))))
       )
     ))
 
 
 (defn get-moves-below "given a hole, row, and game board, returns all moves from below the row.  Moves are returned as possible game boards"
   [hole row game]
-  '()
-  )
+  (if (> row 2)
+    '() ; there are no moves in the last 2 rows going down.
+    (do
+      ;TODO write function that combines this and does the above ones as well... would save a lot of space. pass da function
+      (def left-move (loop [
+                            r row
+                            index (which-element-in-row hole)
+                            move '()]
+                       (if (or (< r 0) (< index 0) (= (count move) 3))
+                         (reverse move)
+                         (recur (+ r 1) index (conj move (which-element-in-game r index))))
+                       ))
+      (def right-move (loop [
+                             r row
+                             index (which-element-in-row hole)
+                             move '()]
+                        (if (or (>= r (count game )) (>= index (count (nth game r))) (= (count move) 3))
+                          (reverse move)
+                          (recur (+ r 1) (+ index 1) (conj move (which-element-in-game r index))))
+                        ))
+      (def movelist (filter #(= 3 (count %)) (list right-move left-move)))
+      (loop [ moves (filter #(= 2 (reduce +
+                                          (let [g (flatten game)]
+                                            (list
+                                              (nth g (first %))
+                                              (nth g (second %))
+                                              (nth g (nth % 2))))))
+                                          movelist)
+              new_games () ]
+        (if ( = (count moves) 0)
+          new_games
+          (recur (rest moves) (conj new_games (split-tree 1 (apply list
+                                                (assoc
+                                                  (assoc  (assoc (vec (flatten game)) (first (first moves)) 1) (second (first moves)) 0)
+                                                  (nth (first moves) 2) 0)
+                                                ))))))
+      )))
 
 (defn get-moves-for-hole "given a game board and specific hole, give all possible moves for that peg"
   [hole game]
-  ;(def a (concat
-  ;  (get-moves-above hole (which-row hole) game)
-  ;  (get-moves-samerow hole (which-row hole) game)
-  ;  (get-moves-below hole (which-row hole) game)
-  ;  ))
-  (println (get-moves-above hole (which-row hole) game))
-  '(1 2 3)
-  )
+  (concat
+    (get-moves-above hole (which-row hole) game)
+    (get-moves-samerow hole (which-row hole) game)
+    (get-moves-below hole (which-row hole) game)
+  ))
 
 (defn get-moves "given a game board, gives all possible moves in the form of the potential game board"
   [game]
   (loop [index 0 moves ()]
-                  (if (= index (- (count (flatten game)) 1))
+
+                  (if (= index (count (flatten game)) )
                     moves ;at this point ,all have been looped through so return the vector of potential game boards
                     (recur
                       (+ index 1)
@@ -151,10 +209,10 @@
   [& args]
   (def game (start-game))
   (println "Initial board" )
-  (print-game game)
-  (get-moves game)
-  ;(print-game '((1) (1 1) (1 1 1) (1 1 1 1) (1 1 0 1 1)))
-  ;(get-moves '((1) (1 1) (1 1 1) (1 1 1 1) (1 1 0 1 1)))
+  ;(print-game game)
+  ;(get-moves game)
+  (print-game '((1) (1 1) (1 0 1) (1 1 1 1) (1 1 1 1 1)))
+  (doall (map print-game (get-moves '((1) (1 1) (1 0 1) (1 1 1 1) (1 1 1 1 1)))))
   )
 
 
